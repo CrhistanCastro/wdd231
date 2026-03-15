5115597c215d803e2b46368b4f17afa386c85ad6
CrhistanCastro <shinobi23senkun@gmail.com>
2026-03-15 15:47:55 -0400

    prophets

---
+++ scripts/async.js
@@ -0,0 +1,9 @@
+const myAsyncFunction = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
